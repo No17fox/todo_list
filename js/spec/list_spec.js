8761994e5934e7575609b4fd8eb87afdefc82137
WangLei <wanglei0717@outlook.com>
2019-01-09 20:10:56 +0800

    Add a method to list class to get active todo

--- js/spec/list_spec.js
+++ js/spec/list_spec.js
@@ -1,16 +1,29 @@
 'use strict';
 
 let List = require("../src/list.js");
+let TodoItems = require("../src/todo_items.js");
+
+let list = new List();
 
 describe("List", () => {
   it("should have a method to add new todo", () => {
-    let list = new List();
     list.addATodo("站会", true);
+
     expect(list.allTodo.get("站会")).toEqual({
       content: "站会", 
       isCompleted: true, 
       isDeleted: false
     });
   });
+
+  it("should have a method to find active todo", () => {
+    list.addATodo("Code review", false);
+
+    let newTodo = new TodoItems("Code review", false);
+    let result = new Map();
+    result.set(newTodo.content, newTodo);
+    
+    expect(list.activeTodo).toEqual(result);
+  })
 })
 
--- js/src/list.js
+++ js/src/list.js
@@ -3,12 +3,21 @@ let TodoItems = require("./todo_items.js");
 class List {
   constructor(allTodo = new Map()) {
     this.allTodo = allTodo;
+    this.activeTodo = this.getActiveTodo();
   }
 
   addATodo(content, isCompleted = false, isDeleted = false) {
     let newTodo = new TodoItems(content, isCompleted, isDeleted);
     this.allTodo.set(content, newTodo);
   }
+
+  getActiveTodo() {
+    return new Map(
+      [...this.allTodo].filter(([key, value]) => value.isCompleted === false)
+    );
+  }
+
+  
 }
 
 module.exports = List;
